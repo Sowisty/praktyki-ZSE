ae7b0b96c17446285cf6977958c3ee6058aa3c6c
unknown <mateusz.malopolski@gmail.com>
2020-10-28 10:53:04 +0100

    pierwszy commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+        int a = 12;
+        double f = 4.5;
+        float af = 5.6f;
+        String s = "hello ";
+        boolean b = true;
+        int[] array = {1,2,3};
+
+        final int c;
+        c = 19;
+
+//        c = 82;
+
+        System.out.println("Hello world! " + s);
+    }
+}
